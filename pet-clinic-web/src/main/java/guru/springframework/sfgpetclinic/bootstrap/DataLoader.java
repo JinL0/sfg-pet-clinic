e6496ff0b417dd93ed5ed6ebb99d10dd7641ce65
jin liu <stuc.cs.jin@berkeley.edu>
2019-09-15 22:12:05 -0700

    add owner/index

--- pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/bootstrap/DataLoader.java
@@ -1,9 +1,8 @@
 package guru.springframework.sfgpetclinic.controllers;
 
+import guru.springframework.sfgpetclinic.model.Owner;
 import guru.springframework.sfgpetclinic.services.OwnerService;
 import guru.springframework.sfgpetclinic.services.VetService;
-import guru.springframework.sfgpetclinic.services.map.OwnerServiceMap;
-import guru.springframework.sfgpetclinic.services.map.VetServiceMap;
 import org.springframework.boot.CommandLineRunner;
 
 public class DataLoader implements CommandLineRunner {
@@ -12,15 +11,27 @@ public class DataLoader implements CommandLineRunner {
     private final VetService vetService;
 
 
-    public DataLoader()
+    public DataLoader(OwnerService ownerService, VetService vetService)
     {
-        ownerService = new OwnerServiceMap();
-        vetService = new VetServiceMap();
+        this.ownerService = ownerService;
+        this.vetService = vetService;
     }
 
     @Override
     public void run(String... args) throws Exception
     {
+        Owner owner1 = new Owner();
+        owner1.setId(1L);
+        owner1.setFirstName("XXX");
+        owner1.setLastName("YYY");
 
+        ownerService.save(owner1);
+
+        Owner owner2 = new Owner();
+        owner2.setId(2L);
+        owner2.setFirstName("owner2");
+        owner2.setLastName("o2F");
+
+        ownerService.save(owner2);
     }
 }
